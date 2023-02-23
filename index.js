03cd9fced5d9ead44378b2eee1eb6ea8833f77c9
bbbux <bmcmalaluan@gmail.com>
2023-02-23 16:44:12 -0600

    Up to 3.17: Added PUT functionality and associated error handling Fixed bug: Update persons list from db before adding new person to ensure no duplicates Remaining bug: After adding, screen is updated just from previous and concat-ing the new entry, instead of refreshing all from db

--- index.js
+++ index.js
@@ -74,13 +74,6 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response, next) => {
-/*     const person = persons.find(person => person.id === +request.params.id);
-
-    if(person) response.json(person);
-    else response.status(404).end(); */
-/*     Person.findById(request.params.id).then(person => {
-        response.json(person);
-    }); */
     Person.findById(request.params.id).then(person => {
         if(person)response.json(person);
         else response.status(404).end();
@@ -92,17 +85,11 @@ app.get('/api/persons/:id', (request, response, next) => {
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
-/*     persons = persons.filter(person => person.id !== +request.params.id);
-    response.status(204).end(); */
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end();
         }).catch(error => next(error))
 });
-
-/* const generateId = () => {
-    return Math.floor(Math.random()*100000);
-}; */
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
@@ -117,14 +104,25 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
 /*         id: generateId() */
     });
-/*     persons = persons.concat(person);
-    response.json(body); */
     person.save().then(savedPerson => {
         response.json(savedPerson);
     });
 });
 
-/* app.put('/api/persons/') */
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    if(!body.name || !body.number) return response.status(400).json(
+        {error: 'Info missing'}
+    );
+    const person = {
+        name: body.name,
+        number: body.number
+    };
+    Person.findByIdAndUpdate(request.params.id, person, {new:true})
+        .then(updatedPerson => response.json(updatedPerson))
+        .catch(error => next(error));
+});
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({error: 'unknown endpoint'})
